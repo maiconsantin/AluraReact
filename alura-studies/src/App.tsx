fdafaf82d971911283d7f5fefdfb1692a96eb631
maiconsantin <maicon_santin@hotmail.com>
2022-12-28 01:16:05 -0300

    Criando primeiros componentes

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Formulario from './components/Formulario/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
