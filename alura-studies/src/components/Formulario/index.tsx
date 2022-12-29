7872a31097d000e541f739069dde8ab56be1f4e1
maiconsantin <maicon_santin@hotmail.com>
2022-12-28 22:52:15 -0300

    Primeiros states Adicionado

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -1,32 +1,47 @@
-import Botao from '../Botao/index';
+import React from 'react';
+import Botao from "../Botao/index";
 import style from "./Formulario.module.scss";
+export default class Formulario extends React.Component {
+  state = {
+    tarefa: "",
+    tempo: "00:00:00",
+  }
 
-export default function Formulario() {
-  return (
-    <form className={style.novaTarefa}>
-      <div className={style.inputContainer}>
-        <label htmlFor="tarefa">Adcione um novo estudo</label>
-        <input
-          type="text"
-          name="tarefa"
-          id="tarefa"
-          placeholder="O que você quer estudar"
-          required
-        />
-      </div>
-      <div className={style.inputContainer}>
-        <label htmlFor="tempo">Tempo</label>
-        <input
-          type="time"
-          step="1"
-          name="tempo"
-          id="tempo"
-          min="00:00:00"
-          max="01:30:00"
-          required
-        />
-      </div>
-      <Botao texto = 'Adicionar' />
-    </form>
-  );
+  adicionaTarefa(evento:React.FormEvent<HTMLFormElement>){
+      evento.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className={style.novaTarefa} onSubmit={this.adicionaTarefa.bind(this)}>
+        <div className={style.inputContainer}>
+          <label htmlFor="tarefa">Adcione um novo estudo</label>
+          <input
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            value={this.state.tarefa}
+            onChange={evento => this.setState({...this.state, tarefa: evento.target.value })}
+            placeholder="O que você quer estudar"
+            required
+          />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="tempo">Tempo</label>
+          <input
+            type="time"
+            step="1"
+            name="tempo"
+            value={this.state.tempo}
+            onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
+            id="tempo"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Botao texto="Adicionar" />
+      </form>
+    );
+  }
 }
--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Item from "./Item";
 import style from "./Lista.module.scss";
 
 export default function Lista() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
       tarefa: "React",
       tempo: "02:00:00",
@@ -15,16 +16,24 @@ export default function Lista() {
       tarefa: "TypeScript",
       tempo: "03:00:00",
     },
-  ];
+  ]);
   return (
     <aside className={style.listaTarefas}>
-      <h2>
-        <ul>
-          {tarefas.map((item, index) => (
-            <Item key={index} {...item} />
-          ))}
-        </ul>
+      <h2
+        onClick={() =>
+          setTarefas([
+            ...tarefas,
+            { tarefa: "Estudar estado", tempo: "05:00:00" },
+          ])
+        }
+      >
+        Estudos do dia
       </h2>
+      <ul>
+        {tarefas.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
     </aside>
   );
 }
