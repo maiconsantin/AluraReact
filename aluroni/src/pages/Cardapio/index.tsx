2ce6ddc0b2504932e50fe3c04be0254d1e1c1e06
maiconsantin <maicon_santin@hotmail.com>
2023-01-08 17:52:43 -0300

    Usando logo com formato svg

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -1,8 +1,11 @@
-export default function Cardapio(){
+import style from './Cardapio.module.scss';
+import {ReactComponent as Logo} from '../../assets/logo.svg'
+
+export default function Cardapio() {
     return (
         <main>
-            <nav className="menu">
-            
+            <nav className={style.menu}>
+                <Logo />
             </nav>
         </main>
     )
