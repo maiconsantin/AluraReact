63b3bd960c5dab2d8c3450744399d7cc32d23630
maiconsantin <maicon_santin@hotmail.com>
2023-01-08 19:13:58 -0300

    Usando scss dado pelo curso e começando o Buscador

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -1,17 +1,29 @@
-import style from './Cardapio.module.scss';
+import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import Buscador from './Buscador/index';
+import { useState } from 'react';
 
 export default function Cardapio() {
+    const [busca, setBusca] = useState("");
     return (
         <main>
-            <nav className={style.menu}>
+            <nav className={styles.menu}>
                 <Logo />
             </nav>
-            <header className={style.header}>
-                <div className={style.header__text}>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
                     A casa do codigo e da massa
                 </div>
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>
+                    Cardapio
+                </h3>
+                <Buscador
+                    busca={busca}
+                    setBusca={setBusca}
+                />
+            </section>
         </main>
     );
 }
